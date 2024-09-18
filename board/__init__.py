95b01ce8659215231807af1e873e23eb9ad80fd9
Izzah Karimah <izzakarimah29@gmail.com>
2024-09-18 18:47:23 +0700

    Update

--- board/__init__.py
+++ board/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
-from board import pages
+from board import pages, database
 
 def create_app():
- app = Flask(__name__)
- app.register_blueprint(pages.bp)
+    app = Flask(__name__)
+    app.config['DATABASE'] = 'board.sqlite'
 
- return app
+    database.init_app(app)
+    app.register_blueprint(pages.bp)
+
+    return app
