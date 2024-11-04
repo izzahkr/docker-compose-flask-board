0c8cd60ad073c8dd6be49bfaf2ed4860ff033679
Izzah Karimah <izzakarimah29@gmail.com>
2024-11-04 09:03:04 +0700

    Database

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,18 @@
 # syntax=docker/dockerfile:1
-<<<<<<< HEAD
-
 FROM python:3.10-alpine
+
 WORKDIR /flask
+
 RUN apk add --no-cache gcc musl-dev linux-headers
+
 COPY requirement.txt requirement.txt
 RUN pip install -r requirement.txt
+
 COPY . .
+
+# Create the data directory for SQLite database
+RUN mkdir -p data
+
 RUN python -m flask --app board init-db
 EXPOSE 5000
 CMD ["python", "-m", "flask", "--app", "board", "run", "--host=0.0.0.0"]
-=======
-FROM python:3.8-slim-buster
-
-WORKDIR /python-docker
-
-COPY requirements.txt requirements.txt
-RUN pip3 install -r requirements.txt
-
-COPY . .
-
-CMD ["python3", "-m", "flask", "run", "--host=0.0.0.0"]
->>>>>>> dev
--- board/__init__.py
+++ board/__init__.py
@@ -1,13 +1,31 @@
+#from flask import Flask
+#from board import pages, database
+#import os
+
+#def create_app():
+#    app = Flask(__name__)
+#    app.config['DATABASE'] = os.path.join(os.getcwd(), 'data', 'board.sqlite')
+
+#    app.config['DEBUG'] = True
+
+#    database.init_app(app)
+#    app.register_blueprint(pages.bp)
+
+#    return app
 from flask import Flask
-from board import pages, database
+from .database import init_app
 
 def create_app():
     app = Flask(__name__)
-    app.config['DATABASE'] = 'board.sqlite'
+    app.config.from_mapping(
+        DATABASE='/home/3323600013_Izzah/flask_board/database.db',
+    )
 
-    app.config['DEBUG'] = True
+    # Register your database functions
+    init_app(app)
 
-    database.init_app(app)
+    # Register blueprints, if any
+    from . import pages
     app.register_blueprint(pages.bp)
 
     return app
